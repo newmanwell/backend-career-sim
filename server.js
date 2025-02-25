337dd2b7bcdb0a753b6e8172a499cd0ad8673fc9
Shaun Newman <shaun.newman.p@gmail.com>
2025-02-25 15:25:23 -0500

    route to add user is functional

--- server.js
+++ server.js
@@ -2,7 +2,24 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8081;
 
+app.use(express.json());
 
+const client = require('./db/client.js');
+client.connect();
+
+const { createUser } = require('./db/users.js');
+
+// Add a user
+app.post('/api/auth/register', async(req, res) => {
+  const { name, password } = req.body;
+
+  try {
+    const newUser = await createUser(name, password);
+    res.send(201);
+  } catch(error) {
+    next(error);
+  }
+});
 
 
 
